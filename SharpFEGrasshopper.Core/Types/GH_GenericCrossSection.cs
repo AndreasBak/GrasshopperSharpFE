4d56ac0336bb0caf7d5aaf6747d733695795f9fb
Iain Sproat <iainsproat@gmail.com>
2013-07-01 19:15:01 +0100

    GH_Beam now creates SharpFE 1DBeam
    1DBeam replaces 3DBeam.

---
+++ SharpFEGrasshopper.Core/Types/GH_GenericCrossSection.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Copyright Iain Sproat, 2013
+ * User: ispro
+ * Date: 01/07/2013
+ * 
+ */
+using System;
+
+namespace SharpGrasshopper.Types
+{
+    /// <summary>
+    /// Description of GH_GenericCrossSection.
+    /// </summary>
+    public class GH_GenericCrossSection
+    {
+        public GH_GenericCrossSection()
+        {
+        }
+    }
+}
