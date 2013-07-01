4a8df42a78088ff4a8391863e8d5664e95dd969c
Iain Sproat <iainsproat@gmail.com>
2013-07-01 17:51:24 +0100

    Tidying project compilation & ignore bin/debug
    ILRepack is now included as a managed package
    When compiling the project it:
    - packages all binary files into a single .gha file using ILRepack and
    moves it to the Grasshopper plugin library  (note: uses a hard-coded
    path to plugin directory so is brittle)
    - No longer attempts to register as a COM object
    - Checks for integer overflow
    - Does not attempt to open Rhino.
    
    The bin and obj folders have been added to the gitignore.

---
+++ SharpFEGrasshopper.Core/Components/GenericCrossSection.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Copyright Iain Sproat, 2013
+ * User: ispro
+ * Date: 01/07/2013
+ * 
+ */
+using System;
+
+namespace SharpGrasshopper.Components
+{
+    /// <summary>
+    /// Description of GenericCrossSection.
+    /// </summary>
+    public class GenericCrossSection
+    {
+        public GenericCrossSection()
+        {
+        }
+    }
+}
